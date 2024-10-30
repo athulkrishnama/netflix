4d8602e38ce9431314d3880a9bf38b081284760b
athulkrishnama <athulkrishnama24@gmail.com>
2024-10-30 16:47:42 +0530

    a row post added in banner

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,20 +1,27 @@
 import React,{useEffect, useState} from 'react'
 import styles from './Banner.module.css';
-import axios, {popular} from '../../axios/axios'
+import axios, {original, popular} from '../../axios/axios'
 import play from '../../assets/play.png'
 import info from '../../assets/info.svg'
+import Rowpost from '../Rowpost/Rowpost';
 function Banner() {
     const [movie, setMovie] = useState({});
+    const [originals, setOriginals] = useState([]);
     useEffect(()=>{
-        axios.get(popular).then((res)=>{
-            const num = Math.floor(Math.random() * (20));
-            const movieObj = {
-                movieName:res.data.results[num].title,
-                movieDescription:res.data.results[num].overview,
-                posterUrl:`https://image.tmdb.org/t/p/original/${res.data.results[num].backdrop_path}`
-            }
-            setMovie(movieObj)
-        })
+    // fetching banner movie
+      axios.get(popular).then((res)=>{
+        const num = Math.floor(Math.random() * (20));
+        const movieObj = {
+          movieName:res.data.results[num].title,
+          movieDescription:res.data.results[num].overview,
+          posterUrl:`https://image.tmdb.org/t/p/original/${res.data.results[num].backdrop_path}`
+        }
+        setMovie(movieObj)
+      })
+      // fetching originals
+      axios.get(original).then((res)=>{
+        setOriginals(res.data.results);
+      })
     },[])
   return (
     <div className={styles.banner} style={{backgroundImage:`linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(9,9,9,0.5830707282913166) 44%, rgba(0,0,0,1) 100%) ,url(${movie.posterUrl})`,}}>
@@ -26,6 +33,7 @@ function Banner() {
             <button className={styles.info}><img src={info} alt="" />More Info</button>
         </div>
       </div>
+      <Rowpost heading="Originals" data={originals} isLarge/>
     </div>
   )
 }
