014443986f0ee38430fad7b2f953d887951139a6
athulkrishnama <athulkrishnama24@gmail.com>
2024-10-30 20:12:52 +0530

    trailer playing while clicking on a movie implemented

--- src/Components/Rowpost/Rowpost.jsx
+++ src/Components/Rowpost/Rowpost.jsx
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import Youtube from 'react-youtube';
 import styles from "./Rowpost.module.css";
+import axios from '../../axios/axios.js'
 function Rowpost({ heading, data, isLarge }) {
-  console.log(data);
+  const [movieId, setMovieId] = useState(null)
+  const opt = {
+    height: '600',
+    width: '100%',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+  const trailerBar = <div>
+    <Youtube  opts={opt} videoId={movieId}/>
+  </div>
+  const handleMovieChange = (id)=>{
+    axios.get(`movie/${id}/videos`).then((res)=>{
+      console.log(res)
+      setMovieId(res.data.results[0]?.key)
+    })
+  }
   return (
     <div
       className={`${styles.rowpost} ${isLarge ? styles.large : styles.small}`}
     >
       <h3>{heading}</h3>
       <div className={styles.movies}>
-        {data?.map((movie, index) => {
-          return <img className={styles.movie} src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`} alt=""/>
+        {data?.map?.((movie, index) => {
+          return <img className={styles.movie} src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`} alt="" onClick={()=>handleMovieChange(movie.id)}/>
         })}
       </div>
+      {movieId && trailerBar}
     </div>
   );
 }
