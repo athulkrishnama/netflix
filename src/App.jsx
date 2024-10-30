bf050d1df5f7074fbd2ca8a529ab8e5a878fcc26
athulkrishnama <athulkrishnama24@gmail.com>
2024-10-30 21:28:18 +0530

    section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from './Components/Navbar/Navbar'
 import Banner from './Components/Banner/Banner'
+import Sections from './Components/Sections/Sections.jsx'
 function App() {
   return (
     <>
      <Navbar/>
      <Banner/>
+     <Sections/>
     </>
   )
 }
