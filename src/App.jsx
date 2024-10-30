b8e800bed3ac65d27f8dbfdf4117c79fe6a032e5
athulkrishnama <athulkrishnama24@gmail.com>
2024-10-30 09:51:03 +0530

    Banner added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Navbar from './Components/Navbar/Navbar'
+import Banner from './Components/Banner/Banner'
 function App() {
   return (
     <>
      <Navbar/>
+     <Banner/>
     </>
   )
 }
