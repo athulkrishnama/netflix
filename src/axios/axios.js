90c89a3fa574085a9faec86044522a87b6255f69
athulkrishnama <athulkrishnama24@gmail.com>
2024-10-30 09:52:06 +0530

    axios initialised

---
+++ src/axios/axios.js
@@ -0,0 +1,9 @@
+import  axios from 'axios';
+import { BASE_URL, API_READ_ACCESS_TOKEN } from '../constants/constants';
+
+export default  axios.create({
+    baseURL: BASE_URL,
+    headers: {'Authorization': `Bearer ${API_READ_ACCESS_TOKEN}`}
+  });
+
+export const popular = 'movie/popular'
